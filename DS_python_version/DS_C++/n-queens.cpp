d89e7a7f943a81e6173340b9d270fcc4e0bd34a0
happyflying-web <1962462181@qq.com>
2021-02-05 19:02:00 +0800

    1

---
+++ DS_python_version/DS_C++/n-queens.cpp
@@ -0,0 +1,10 @@
+// https://leetcode-cn.com/problems/n-queens/
+#include<vector>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+
+    }
+};
---
+++ DS_python_version/DS_C++/powx-n.cpp
@@ -0,0 +1,7 @@
+// https://leetcode-cn.com/problems/powx-n/
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return pow(x,n);
+    }
+};
